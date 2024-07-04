57708be16936e69ce0fee603d40a44fd2e97d7d9
Aymane Mehdi 🇵🇸 <unlocked.dark.web@gmail.com>
2024-07-05 00:44:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,18 +9,9 @@ const LoginRoute = require('./routes/loginRoutes');
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://www.saidtex.ma', // Replace with your actual domain
-    methods: ['GET', 'POST'], // Specify allowed HTTP methods
-    allowedHeaders: ['Content-Type'], // Specify allowed headers
-};
+app.use(cors); 
 
-app.use(cors(corsOptions)); // Enable CORS with custom options
-
-// MongoDB Connection
-mongoose.connect(process.env.URL, {
-useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+mongoose.connect(process.env.URL)
 .then(() => {
     console.log('Successfully connected to MongoDB');
 })
@@ -34,7 +25,7 @@ app.use(middlewareLog);
 app.use("/partners", PartnerRoute);
 app.use("/login", LoginRoute);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
