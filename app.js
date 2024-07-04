5e47b143e6761a1118f52262cf58f34c2fa364cd
Aymane Mehdi 🇵🇸 <unlocked.dark.web@gmail.com>
2024-07-05 00:27:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 const middlewareLog = require('./middlewares/middleware');
@@ -9,22 +8,29 @@ const PartnerRoute = require('./routes/partnerRoutes');
 const LoginRoute = require('./routes/loginRoutes');
 
 const app = express();
-app.use(cors());
 
-mongoose.connect(process.env.URL)
-    .then(() => {
-        console.log('Successfully connected to MongoDB');
-    })
-    .catch(error => {
-        console.error('Connection error:', error.message);
-    });
+// Middleware
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON bodies
+app.use(middlewareLog); // Custom middleware for logging
 
-app.use(express.json());
-app.use(middlewareLog);
+// MongoDB Connection
+mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Successfully connected to MongoDB');
+})
+.catch(error => {
+    console.error('Connection error:', error.message);
+});
 
-app.use("/partners", PartnerRoute);
-app.use("/login", LoginRoute);
+// Routes
+app.use("/partners", PartnerRoute); // Example route for partners
+app.use("/login", LoginRoute); // Example route for login
 
+// Server setup
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
