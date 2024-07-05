c91dc9d0c0138e2b2e7dd7565211dcbe5a875c9e
Aymane Mehdi 🇵🇸 <unlocked.dark.web@gmail.com>
2024-07-05 01:01:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ const middlewareLog = require('./middlewares/middleware');
 const PartnerRoute = require('./routes/partnerRoutes');
 const LoginRoute = require('./routes/loginRoutes');
 
-require("./Config/db");
-
 const app = express();
+app.use(cors());
 
-app.use(cors); 
+mongoose.connect(process.env.URL)
+  .then(() => console.log("Connected to database"))
+  .catch((error) => console.log("Error: ", error));
 
 app.use(express.json());
+
 app.use(middlewareLog);
 
-app.use("/partners", PartnerRoute);
-app.use("/login", LoginRoute);
+app.use("/blogs",BlogRoute);
+app.use("/login",LoginRoute);
 
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
