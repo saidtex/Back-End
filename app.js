e29c42d4db98d95a715c12e42f03c3651252ea41
Aymane Mehdi 🇵🇸 <unlocked.dark.web@gmail.com>
2024-07-05 01:55:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 const url = process.env.URL;
 const port = process.env.PORT;
 
 const middlewareLog = require('./middlewares/middleware');
-
-const PartnerRoute = require('./routes/partnerRoutes')
-const LoginRoute = require('./routes/loginRoutes')
-
+const PartnerRoute = require('./routes/partnerRoutes');
+const LoginRoute = require('./routes/loginRoutes');
 
 const app = express();
 app.use(cors());
-
-mongoose.connect(url)
-  .then(() => console.log("Connected to database"))
-  .catch((error) => console.log("Error: ", error));
-
 app.use(express.json());
 
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to database");
+    
+    // Start listening once connected
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to database: ", error);
+  });
+
+// Middleware for logging
 app.use(middlewareLog);
 
-app.use("/partners",PartnerRoute);
-app.use("/login",LoginRoute);
+// Routes
+app.use("/partners", PartnerRoute);
+app.use("/login", LoginRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
