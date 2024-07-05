ae96bef1496d8b9ea6aaeadb5d24b9fb213b96e5
Aymane Mehdi 🇵🇸 <unlocked.dark.web@gmail.com>
2024-07-05 02:34:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,18 +14,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
+// Async function to connect to MongoDB
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("Connected to database");
-    
+  } catch (error) {
+    console.error("Error connecting to database: ", error);
+    // Exit the process if unable to connect to MongoDB
+    process.exit(1);
+  }
+};
+
+// Connect to MongoDB and start server
+const startServer = async () => {
+  try {
+    await connectToDatabase();
     // Start listening once connected
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
-  })
-  .catch((error) => {
-    console.error("Error connecting to database: ", error);
-  });
+  } catch (error) {
+    console.error("Error starting server: ", error);
+    // Exit the process if there's an error starting the server
+    process.exit(1);
+  }
+};
+
+// Call the function to start server and connect to MongoDB
+startServer();
 
 // Middleware for logging
 app.use(middlewareLog);
